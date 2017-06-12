8d702846e17394f779c9bfa81f654240d5b49e78
PriitParmakson <priit.parmakson@gmail.com>
2017-06-12 03:40:55 +0300

    Lisatud küpsiste töötlus

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 'use strict';
 
+/* Märkmed
+
+ * Pärime skoobid 'user' ja 'public_repo'. Vt https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/about-scopes-for-oauth-apps/
+
+ */
+
+
+/* 1 Sõltuvuste importimine
+   ------------------------------------------
+*/
+
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const simpleOauthModule = require('simple-oauth2');
 const uid = require('rand-token').uid;
 const requestModule = require('request');
 
+/* 2 Objektide loomine ja konfigureerimine 
+   -----------------------------------------
+*/
+
 // Veebiserveri ettevalmistamine
 const app = express();
+app.use(cookieParser());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
@@ -28,14 +45,16 @@ const oauth2 = simpleOauthModule.create({
 // Generate a 16 character alpha-numeric token:
 var token = uid(16);
 
-/* Pärime skoobid 'user' ja 'public_repo'. Vt https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/about-scopes-for-oauth-apps/ */
-
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
   redirect_uri: 'https://samategev.herokuapp.com/OAuthCallback',
   scope: 'user public_repo',
   state: token,
 });
+
+/* 3 Marsruutimine
+   -------------------------------------------
+*/
 
 // Suunamine rakendusele õigust andma (Githubis)
 app.get('/auth', (req, res) => {
@@ -52,53 +71,48 @@ app.get('/OAuthCallback', (req, res) => {
 
   oauth2.authorizationCode.getToken(options, (error, result) => {
     if (error) {
-      console.error('Access Token Error', error.message);
-      return res.json('Authentication failed');
+      console.error('Viga juurdepääsutõendi pärimisel', error.message);
+      return res.json('Autentimine ebaõnnestus');
     }
-
-    console.log('The resulting token: ', result);
+    console.log('==Juurdepääsutõend: ', result);
     // Selgitada, milleks järgnev kasulik on
     const token = oauth2.accessToken.create(result);
-
     // Juurdepääsutõendi saatmine küpsisesse panekuks - ettevalmistus
     res.cookie('GHtoend', JSON.stringify(token));
-
-    /* Tuleks tagastada kasutaja nimi
-       Kõigepealt pärida kasutaja nime Github-st
-      Vt. https://developer.github.com/v3/users/#get-the-authenticated-user
-      
-    */
-    console.log('==Juurdepääsutõend: ' + result.access_token);
-    const GithubAPIURL = 'https://api.github.com/';
-    var options = {
-      url: GithubAPIURL + 'user',
-      headers: {
-        'User-Agent': 'Samatekst',
-        'Authorization': 'token ' + result.access_token
-      }
-    };
-    requestModule(
-      options,
-      function (error, response, body) {
-        if (error) {
-          console.log('Viga kasutaja andmete pärimisel Github-st: ', error);  
-        }
-        if (response) {
-          console.log('Kasutaja andmete päring Github-st - statusCode: ', response.statusCode);  
-        }
-        console.log('body: ', body);
-        var saadudAndmed = JSON.parse(body);
-        console.log('kasutaja: ', saadudAndmed.login);
-        res.status(200)
-          .render('pages/autenditud', { kasutaja: saadudAndmed.login });
-    });
-
-
-    // Saadab päringuvastuses juurdepääsutõendi, kuvamiseks   
-    /* return res
-      .status(200)
-      .json(token); */
+    res.redirect('/autenditud');
   });
+});
+
+app.get('/autenditud', (req, res) => {
+  /* Juurdepääsutõendi väljavõtmine päringuga saadetud küpsisest */
+  const GHtoend = req.cookies.GHtoend;
+  console.log('GHtoend: ' + GHtoend);
+  const access_token = JSON.parse(GHtoend).access_token;
+  console.log('Ekstraheeritud juurdepääsutõend: ' + access_token);
+  /* Pärime kasutaja nime Github-st Vt. https://developer.github.com/v3/users/#get-the-authenticated-user
+  */
+  const GithubAPIURL = 'https://api.github.com/';
+  var options = {
+    url: GithubAPIURL + 'user',
+    headers: {
+      'User-Agent': 'Samatekst',
+      'Authorization': 'token ' + access_token
+    }
+  };
+  requestModule(
+    options,
+    function (error, response, body) {
+      if (error) {
+        console.log('Viga kasutaja andmete pärimisel Github-st: ', error);
+      }
+      if (response) {
+        console.log('Kasutaja andmete päring Github-st - statusCode: ', response.statusCode);
+      }
+      var saadudAndmed = JSON.parse(body);
+      console.log('kasutaja: ', saadudAndmed.login);
+      res.status(200)
+        .render('pages/autenditud', { kasutaja: saadudAndmed.login });
+    });
 });
 
 // Selgitada, mida see teeb
@@ -111,10 +125,13 @@ app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-// Veebiserveri käivitamine
+/* 4 Käimatõmbamine
+   -------------------------------------------
+*/
 
+// Veebiserveri käivitamine
 app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node rakendus töötab, port', app.get('port'));
 });
 
 
